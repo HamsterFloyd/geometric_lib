9944bd528d65270f1eb00c7374488107d461d601
HamsterFloyd <pravashinskiy@gmail.com>
2025-11-04 07:21:59 +0300

    добавил треугольник и прямоугольник

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    #Функция принимает на вход радиус, возвращает площадь круга
     return math.pi * r * r
 
 
 def perimeter(r):
+    #Функция принимает на вход радиус, возвращает периметр круга
     return 2 * math.pi * r
 
